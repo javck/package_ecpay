c2a9fd6076f02065387805f1fde3e9d04a2da278
Lin Pin Chueh <javck.zack@gmail.com>
2019-08-10 16:36:53 +0800

    bug fix

--- routes/ecpay.php
+++ routes/ecpay.php
@@ -6,3 +6,10 @@ Route::prefix('ecpay')->group(function(){
     Route::post('return', 'App\Http\Controllers\EcPayController@returnUrl')
         ->name('ecpay.return');
 });
+
+Route::group(['middleware' => ['javck.checkForMaintenanceMode','web'],'prefix' => 'ecpay','namespace' => '\App\Http\Controllers'], function () {
+    Route::get('createOrder/{id}','SiteController@createPaymentOrderPage');
+    Route::get('request','SiteController@paymentRequest')->name('payment.request');
+    Route::post('submitCheckout','SiteController@createPaymentOrder');
+    Route::post('done/{order_id}','SiteController@paymentReturn');
+});
